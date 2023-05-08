c8922bca99a10ce12d623d6786488fa20430c225
Fedor <fedoravdeev3@gmail.com>
2023-05-08 17:56:25 +0700

    done opening image with alignment

--- project_photo/static/script/index.js
+++
@@ -1,7 +0,0 @@
-// document.addEventListener("DOMContentLoaded", handlerDOMContentLoaded)
-
-// function handlerDOMContentLoaded() {
-//   const footer = document.getElementById('footer')
-//   console.error(footer)
-//   footer.style.position = 'absolute';
-// }
--- project_photo/static/script/main.js
+++ project_photo/static/script/main.js
@@ -1,10 +1,13 @@
 let photoPopup
+let photoOrigin
 
 document.addEventListener('DOMContentLoaded', handlerDOMContentLoaded)
 
 function handlerDOMContentLoaded() {
   const photos = document.getElementsByClassName('photo')
   const close = document.getElementById('close')
+  photoPopup = document.getElementById('photo-popup')
+  photoOrigin = document.getElementById('photo-origin')
   for(const photo of photos){ 
     photo.addEventListener('click', handlerClickOnPhoto)
   }
@@ -12,23 +15,13 @@ function handlerDOMContentLoaded() {
 }
 
 function handlerClickOnPhoto(elem) {
-  console.log(elem.target.style.backgroundImage)
-  photoPopup = document.getElementById('photo-popup')
+  const backgroundImage = elem.target.style.backgroundImage
+  const to = backgroundImage.length - 2
+  url = backgroundImage.substring(5, to)
+  photoOrigin.src = url
   photoPopup.style.display='flex'
 }
 
 function handlerClickOnClose() {
   photoPopup.style.display='none'
-}
-
-function sendingRequestToServer(method, url) {
-  const Request = new XMLHttpRequest()
-  Request.open(method, url, true)
-  // Request.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8')
-  Request.onreadystatechange = () => {
-    if (Request.readyState == 4) {
-      console.log('sending to the server was successful')
-    }
-  }
-  Request.send()
 }
